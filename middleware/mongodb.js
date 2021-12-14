36304f4d72488baa019b27068d57c47f8dd26a33
Yann Decelle <yann.decelle@edu.devinci.fr>
2021-12-14 16:26:38 +0100

    resolved errors

--- middleware/mongodb.js
+++ middleware/mongodb.js
@@ -1,14 +1,14 @@
-import { connection, mongoose } from "mongoose";
+import mongoose from "mongoose";
 
 const connectDB = handler => async (req, res) => {
     // if (mongoose.connection[0].readyState) {
     //     return handler(req, res)
     // }
 
-    await mongoose.connect(process.env.mongouri, {
+    await mongoose.connect(process.env.YannDCL, {
         useUnfifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
+        // useFindAndModify: false,
+        // useCreateIndex: true,
         usenewurlParser: true
     });
     return handler(req, res)
--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  env:{
+    YannDCL : 'mongodb+srv://YannDcl:1234@cluster0.apnfl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  }
 }
